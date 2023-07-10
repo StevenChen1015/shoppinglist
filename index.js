cf334a003f4214549886d71aee496ad0eee95539
Steven Chen <schen53@mylangara.bc.ca>
2023-07-10 14:52:30 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    const tableBody = document.querySelector('#itemsData tbody');
+
+    data.forEach(group => {
+      const { type, items } = group;
+      items.forEach((item, index) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+          <td>${index === 0 ? type : ''}</td>
+          <td>${item.name}</td>
+          <td>${item.description}</td>
+        `;
+        tableBody.appendChild(row);
+      });
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
